457756e990607446b9386cefc0f55a7504455757
justapig9020 <justapig9020@gmail.com>
2023-08-31 20:01:07 +0800

    Add unittest for "with_fail"

--- test/test.py
+++ test/test.py
@@ -6,13 +6,24 @@ from judge import judge
 import unittest
 
 class Test(unittest.TestCase):
-    def test(self):
+    def test_all_pass(self):
         report = judge('all_pass/judge.yaml')
         self.assertEqual(len(report), 5)
         # Traverse the value of report
         # Assert that all values are None
         for result in report.values():
             self.assertEqual(result, None, result)
+    
+    def test_with_fail(self):
+        report = judge('with_fail/judge.yaml')
+        self.assertEqual(len(report), 5)
+        # Traverse the value of report
+        # Assert that all values are None except the "input_5"
+        for testcase, result in report.items():
+            if 'input_5' in testcase:
+                self.assertNotEqual(result, None, result)
+            else:
+                self.assertEqual(result, None, result)
 
 if __name__ == '__main__':
     unittest.main()
