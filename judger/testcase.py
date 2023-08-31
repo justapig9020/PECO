3115021ed8eab6840d15b647d66b4d6b6b0c7afe
justapig9020 <justapig9020@gmail.com>
2023-08-31 20:51:38 +0800

    Raise exception if input or expect file misses

--- judger/testcase.py
+++ judger/testcase.py
@@ -8,6 +8,31 @@ class IndexedFile:
         self.index = index
         self.name = name
 
+class InputMiss(Exception):
+    def __init__(self, expect_file):
+        super().__init__(f'Input file for {expect_file} is missing')
+
+class ExpectMiss(Exception):
+    def __init__(self, input_file):
+        super().__init__(f'Expect file for {input_file} is missing')
+
+def expect_file(input_file, expect_hash):
+    if input_file.index in expect_hash:
+        return expect_hash.pop(input_file.index).name
+    else:
+        raise ExpectMiss(input_file.name)
+
+def build_testcases(input_list, expect_list):
+    expect_hash = {file.index: file for file in expect_list}
+
+    testcases = [(input.name, expect_file(input, expect_hash)) for input in input_list]
+
+    if len(expect_hash) != 0:
+        # pick one of the expect file
+        input_miss = list(expect_hash.values())[0]
+        raise InputMiss(input_miss.name)
+    
+    return testcases
 
 def list_testcases(config):
     path = config['root'] + '/' + variable.solve_string(config, config['testcases']['path'])
@@ -19,12 +44,13 @@ def list_testcases(config):
     files = list_files_recursively(path)
     input_list = list_matched_files(files, input_format_re)
     expect_list = list_matched_files(files, expect_format_re)
-    input_list.sort(key=lambda x: x.index)
-    expect_list.sort(key=lambda x: x.index)
 
-    id_format_re = '(' + variable.solve_string(config, input_format) + ')'
-    id = re.compile(id_format_re)
-    return [(input.name, expect.name, id.findall(input.name)[0]) for input, expect in zip(input_list, expect_list)]
+    testcases = build_testcases(input_list, expect_list)
+
+    file_format_re = '(' + variable.solve_string(config, input_format) + ')'
+    file_name = re.compile(file_format_re)
+    testcases = [(input, expect, file_name.findall(input)[0]) for input, expect in testcases]
+    return testcases
 
 def list_files_recursively(path):
     result = []
@@ -39,4 +65,4 @@ def list_matched_files(files, format):
     format = re.compile(format)
     files = [file for file in files]
     indexed_files = [(file, format.findall(file)) for file in files]
-    return [IndexedFile(index, file) for file, index in indexed_files if index != []]
+    return [IndexedFile(index[0], file) for file, index in indexed_files if index != []]
--- test/test.py
+++ test/test.py
@@ -4,17 +4,30 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../judger")
 
 from judge import judge
 import unittest
+import re
 
 class Test(unittest.TestCase):
+    def setUp(self):
+        self.pwd = os.getcwd()
+
+    def tearDown(self):
+        os.chdir(self.pwd)
+
     def test_all_pass(self):
+        pwd = os.getcwd()
+
         report = judge('all_pass/judge.yaml')
         self.assertEqual(len(report), 5)
         # Traverse the value of report
         # Assert that all values are None
         for result in report.values():
             self.assertEqual(result, None, result)
-    
+
+        self.assertEqual(os.getcwd(), pwd)
+
     def test_with_fail(self):
+        pwd = os.getcwd()
+
         report = judge('with_fail/judge.yaml')
         self.assertEqual(len(report), 5)
         # Traverse the value of report
@@ -24,11 +37,33 @@ class Test(unittest.TestCase):
                 self.assertNotEqual(result, None, result)
             else:
                 self.assertEqual(result, None, result)
-    
+
+        self.assertEqual(os.getcwd(), pwd)
+
     def test_compile_error(self):
+        pwd = os.getcwd()
+
         report = judge('compile_error/judge.yaml')
         self.assertEqual(len(report), 1)
         self.assertTrue('Compile Error' in report)
+
+        self.assertEqual(os.getcwd(), pwd)
+
+    def test_missing_input(self):
+        from testcase import InputMiss
+        with self.assertRaises(InputMiss) as raised:
+            judge('missing_input/judge.yaml')
+        expect = re.compile("Input file for .*missing_input/testcases/output_1.txt is missing")
+        message = f'{raised.exception}'
+        self.assertIsNotNone(expect.match(message), message)
+
+    def test_missing_expect(self):
+        from testcase import ExpectMiss
+        with self.assertRaises(ExpectMiss) as raised:
+            judge('missing_expect/judge.yaml')
+        expect = re.compile("Expect file for .*missing_expect/testcases/input_1.txt is missing")
+        message = f'{raised.exception}'
+        self.assertIsNotNone(expect.match(message), message)
 
 if __name__ == '__main__':
     unittest.main()
