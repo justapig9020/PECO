47d2ea16b84164abca29e82ff5a754aeda250769
justapig9020 <justapig9020@gmail.com>
2023-09-02 16:43:44 +0800

    Add dict as a new report type

--- judger/main.py
+++ judger/main.py
@@ -11,8 +11,13 @@ if __name__ == '__main__':
     # the argument "quiet", or short in "q", is a flag, if it is set, the report will not contain the log of the commands and without colors
     parser.add_argument('-q', '--quiet', help='quiet mode, do not print the log of the commands', action='store_true', default=False)
 
+    parser.add_argument('-d', '--dict', help='report as a dict', action='store_true', default=False)
+
     args = parser.parse_args()
 
     results = judge(args.config)
 
-    print(report(results, verbose = not args.quiet))
+    if args.dict:
+        print(results)
+    else:
+        print(report(results, verbose = not args.quiet))
